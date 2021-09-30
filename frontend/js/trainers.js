b683ef41bf7ea4bafbfba3d23d1b183360014861
Au Yong Ting Ting <ouyangtingting.j@gmail.com>
2021-09-30 22:32:27 +0800

    Creation of trainers and amendments of learners

--- frontend/js/trainers.js
+++ frontend/js/trainers.js
@@ -1,5 +1,5 @@
-function getAllRegisteredLearners() {
-    document.getElementById("learners_list").innerHTML = "";
+function getAllUnassignedCourses() {
+    document.getElementById("trainers_list").innerHTML = "";
 
 
     var request = new XMLHttpRequest();
@@ -7,96 +7,57 @@ function getAllRegisteredLearners() {
     request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var obj = JSON.parse(this.response);
-            var registered = obj.registered_learners;
-            console.log(registered);
+            var unassigned = obj.unassigned_courses;
+            console.log(unassigned);
 
             var record = '';
-            for (var i = 0; i < registered.length; i++) {
-                var name = registered[i].name;
-                var id = registered[i].id;
-                var apply_datetime = registered[i].apply_datetime;
-                var class_section = registered[i].class;
+            for (var i = 0; i < unassigned.length; i++) {
+                var course_name = unassigned[i].course_name;
+                var class_id = unassigned[i].class_id;
+                var start_date = unassigned[i].start_date;
 
                 record += `<tr>
-                    <td class="text-center align-middle">${name}</td>
-                    <td class="text-center align-middle">${id}</td>
-                    <td class="text-center align-middle">${apply_datetime}</td>
-                    <td class="text-center align-middle">${class_section}</td>
-                    <td class="text-center align-middle my-auto"><p class='btn-success btn rounded-pill align-middle my-auto'>Approve</p></td>
-                  </tr>`;
-            }
-            document.getElementById("learners_list").innerHTML = record;
-        }
-    };
-
-    url = "../hardcoded.json";
-    request.open("GET", url, true);
-    request.send();
-};
-
-function getAllEnrolledLearners() {
-    document.getElementById("learners_list").innerHTML = "";
-
-    var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var obj = JSON.parse(this.response);
-            var registered = obj.enrolled_learners;
-            console.log(registered);
-
-            var record = '';
-            for (var i = 0; i < registered.length; i++) {
-                var name = registered[i].name;
-                var id = registered[i].id;
-                var apply_datetime = registered[i].apply_datetime;
-                var class_section = registered[i].class;
-
-
-                record += `<tr>
-                    <td class="text-center align-middle">${name}</td>
-                    <td class="text-center align-middle">${id}</td>
-                    <td class="text-center align-middle">${apply_datetime}</td>
-                    <td class="text-center align-middle">${class_section}</td>
-                    <td class="text-center align-middle my-auto"><p class='btn-danger btn rounded-pill align-middle my-auto'>Withdraw</p></td>
-                  </tr>`;
-            }
-            document.getElementById("learners_list").innerHTML = record;
-        }
-    };
-
-    url = "../hardcoded.json";
-    request.open("GET", url, true);
-    request.send();
-};
-
-function getAllPreassignLearners() {
-    document.getElementById("learners_list").innerHTML = "";
-
-    var request = new XMLHttpRequest();
-
-    request.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var obj = JSON.parse(this.response);
-            var registered = obj.preassign_learners;
-            console.log(registered);
-
-            var record = '';
-            for (var i = 0; i < registered.length; i++) {
-                var name = registered[i].name;
-                var id = registered[i].id;
-                var apply_deadline = registered[i].apply_deadline;
-                var class_section = registered[i].class;
-
-                record += `<tr>
-                    <td class="text-center align-middle">${name}</td>
-                    <td class="text-center align-middle">${id}</td>
-                    <td class="text-center align-middle">${apply_deadline}</td>
-                    <td class="text-center align-middle">${class_section}</td>
+                    <td class="text-center align-middle">${course_name}</td>
+                    <td class="text-center align-middle">${class_id}</td>
+                    <td class="text-center align-middle">${start_date}</td>
                     <td class="text-center align-middle my-auto"><p class='btn-success btn rounded-pill align-middle my-auto'>Assign</p></td>
                   </tr>`;
             }
-            document.getElementById("learners_list").innerHTML = record;
+            document.getElementById("trainers_list").innerHTML = record;
+        }
+    };
+
+    url = "../hardcoded.json";
+    request.open("GET", url, true);
+    request.send();
+};
+
+function getAllAssignedCourses() {
+    document.getElementById("trainers_list").innerHTML = "";
+
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.response);
+            var assigned = obj.assigned_courses;
+            console.log(assigned);
+
+            var record = '';
+            for (var i = 0; i < assigned.length; i++) {
+                var course_name = assigned[i].course_name;
+                var class_id = assigned[i].class_id;
+                var start_date = assigned[i].start_date;
+
+
+                record += `<tr>
+                    <td class="text-center align-middle">${course_name}</td>
+                    <td class="text-center align-middle">${class_id}</td>
+                    <td class="text-center align-middle">${start_date}</td>
+                    <td class="text-center align-middle my-auto"><p class='btn-danger btn rounded-pill align-middle my-auto'>Unassign</p></td>
+                  </tr>`;
+            }
+            document.getElementById("trainers_list").innerHTML = record;
         }
     };
 
