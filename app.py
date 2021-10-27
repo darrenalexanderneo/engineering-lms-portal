73b2544b5c25a2bf81c7222715a15f104bd3082e
Darren Neo <darrenalexanderneo@gmail.com>
2021-10-27 20:06:57 +0800

    Created a testing endpoint to test Staging Heroku

--- app.py
+++ app.py
@@ -1473,7 +1473,13 @@ def create_quiz():
             
         })
 
-
+@app.route("/testHeroku") 
+def test_heroku():
+    return jsonify(
+        {
+            "code": "All's good!"
+        }
+    )
 
 
 if __name__ == '__main__':
