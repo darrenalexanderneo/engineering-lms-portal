1f6265359e79bf89487af6b05397acffd42918da
Darren Neo <darrenalexanderneo@gmail.com>
2021-10-27 12:39:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from datetime import datetime
 # EC2 DB port is 3306 instead, change accordingly.
 app = Flask(__name__)
 # EC2 DB port is 3306 instead, change accordingly.
-# app.config['SQLALCHEMY_DATABASE_URI'] = config('dbURL') or environ.get("dbURL")
-app.config['SQLALCHEMY_DATABASE_URI'] = config('localURL') or environ.get('localURL')
+app.config['SQLALCHEMY_DATABASE_URI'] = config('dbURL') or environ.get("dbURL")
+# app.config['SQLALCHEMY_DATABASE_URI'] = config('localURL') or environ.get('localURL')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
