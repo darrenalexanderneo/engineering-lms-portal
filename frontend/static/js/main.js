1ea3aa53da5bd1122af5b32d1d773516960e0a9b
veronicateng13 <veronicatengyy@gmail.com>
2021-10-28 21:28:58 +0800

    added create_quiz page
    to do
    - posting the request
    - making sure that inputs are retained even when new questions are added
    - view_quiz.html (waiting for backend)

---
+++ frontend/static/js/main.js
@@ -0,0 +1,30 @@
+/*** RETRIEVE API KEYS ***/
+function getAPIkeys() {  
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var api_keys = JSON.parse(this.response);
+            getCourseList_TNR = api_keys.getCourseList_TNR;
+            getCourseDetails_TNR = api_keys.getCourseDetails_TNR;
+            createQuiz_POST_TNR = api_keys.createQuiz_POST_TNR;
+
+            // console.log(getCourseList_TNR);
+            // console.log(getCourseDetails_TNR);
+            // console.log(createQuiz_POST_TNR);
+        }
+    }
+    request.open("GET", "../../apikey.json", false);
+    request.send();
+}
+
+/*** REDIRECT TO PAGE ***/
+function redirectToPage(given_page) {
+    setTimeout(function () { 
+        window.location.replace(`${given_page}.html`);
+    }, 700);
+}
+
+function backToPrevPage(given_page) {
+    window.location.replace(`${given_page}.html`);
+    // storage.clear();
+}
