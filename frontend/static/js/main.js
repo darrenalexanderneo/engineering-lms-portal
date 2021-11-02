fb8496c9a834235bde6422783d93bf9a884a1bf7
veronicateng13 <veronicatengyy@gmail.com>
2021-11-02 23:12:45 +0800

    edited hr pages

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,7 +1,8 @@
-/*** CHANGE ACCORDINGLY ***/
 const development = 'apikey_development.json';
 const production = 'apikey_production.json';
 
+/*** CHANGE ACCORDINGLY ***/
+const apikey_url = development;
 
 /*** RETRIEVE API KEYS ***/
 function getAPIkeys_TNR() {  
@@ -13,6 +14,27 @@ function getAPIkeys_TNR() {
             getCourseDetails_TNR = api_keys.getCourseDetails_TNR;
             viewQuiz_TNR = api_keys.viewQuiz_TNR;
             createQuiz_POST_TNR = api_keys.createQuiz_POST_TNR;
+
+            // console.log(getCourseList_TNR);
+            // console.log(getCourseDetails_TNR);
+            // console.log(viewQuiz_TNR);
+            // console.log(createQuiz_POST_TNR);
+        }
+    }
+    request.open("GET", `../../${apikey_url}`, false);
+    request.send();
+}
+
+function getAPIkeys_HR() {  
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var api_keys = JSON.parse(this.response);
+            getCourseList_HR = api_keys.getCourseList_HR;
+            getCourseDetails_HR = api_keys.getCourseDetails_HR;
+            getLearnerList_HR = api_keys.getLearnerList_HR;
+            assignLearner_POST_HR = api_keys.assignLearner_POST_HR;
+            withdrawLearner_POST_HR = api_keys.withdrawLearner_POST_HR;
 
             // console.log(getCourseList_TNR);
             // console.log(getCourseDetails_TNR);
